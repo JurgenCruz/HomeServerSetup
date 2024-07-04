c8297b3a24752c494f4c32a594ade5eaa35e75bb
Jurgen Cruz <jurgenacc@tutanota.com>
2024-07-03 21:16:03 -0400

    Add SELinux policy for flaresolverr
    Chromium was failing due to SELinux

--- scripts/selinux_setup.sh
+++ scripts/selinux_setup.sh
@@ -6,4 +6,4 @@ setsebool -P virt_sandbox_use_netlink 1
 setsebool -P domain_kernel_load_modules 1
 setsebool -P container_use_devices 1
 setsebool -P rsync_full_access 1
-semodule -i ./files/{arr.cil,qbittorrent.cil,wireguard.cil,rsync.cil} /usr/share/udica/templates/{base_container.cil,net_container.cil}
+semodule -i ./files/{arr.cil,qbittorrent.cil,flaresolverr.cil,wireguard.cil,rsync.cil} /usr/share/udica/templates/{base_container.cil,net_container.cil}
