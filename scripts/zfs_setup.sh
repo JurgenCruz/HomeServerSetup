dc7a51314d179d762272909ac954ae8c1fd7bc85
Jurgen Cruz <jurgenacc@tutanota.com>
2024-07-03 21:16:27 -0400

    Update ZFS repo URL

--- scripts/zfs_setup.sh
+++ scripts/zfs_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dnf install -y "https://zfsonlinux.org/fedora/zfs-release-2-4$(rpm --eval "%{dist}").noarch.rpm" # Add ZFS repo
+dnf install -y "https://zfsonlinux.org/fedora/zfs-release-2-5$(rpm --eval "%{dist}").noarch.rpm" # Add ZFS repo
 # ZFS is built for your specific kernel using DKMS and loaded dynamically by your kernel at boot
 dnf install -y zfs
 # Queue the registration of DKMS keys for secure boot. Otherwise, the kernel will reject loading the ZFS module
