68bd4db163a66c18a88806e8ebdc9c23869ddbf7
Jurgen Cruz <jurgenacc@tutanota.com>
2024-11-04 09:01:23 -0500

    Redirect curl error to log file to diagnose duck.sh

--- scripts/duck.sh
+++ scripts/duck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo url="https://www.duckdns.org/update?domains=XXX&token=YYY&ip=" | curl -s -S -k -o ~/duck.log -K -
+curl -s -S -o ~/duck.log "https://www.duckdns.org/update?domains=XXX&token=YYY&ip=" 2> ~/duck_status.log
