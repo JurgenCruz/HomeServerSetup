8b89acb200b97d420236f9d6135d6838d7ad0a7c
Jurgen Cruz <jurgenacc@tutanota.com>
2025-03-18 22:38:49 -0400

    Update cockpit script with new packages
    - cockpit-pcp is deprecated in favor of python3-pcp

--- scripts/cockpit_setup.sh
+++ scripts/cockpit_setup.sh
@@ -4,6 +4,6 @@ dnf install -y cockpit git
 systemctl enable --now cockpit.socket
 firewall-cmd --permanent --add-service=cockpit
 firewall-cmd --reload
-dnf install -y cockpit-pcp cockpit-packagekit cockpit-navigator
+dnf install -y cockpit-packagekit cockpit-navigator python3-pcp
 git clone https://github.com/45drives/cockpit-zfs-manager.git
 cp -r cockpit-zfs-manager/zfs /usr/share/cockpit/
