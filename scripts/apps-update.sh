95e72fe0248c442f0cd7ff7b637532d1c8573b4b
Jurgen Cruz <jurgenacc@tutanota.com>
2026-03-04 22:14:00 -0500

    Skip updating stacks that are down
    - Detect a down stack by checking if all services are down
    - Skip downed stacks to prevent uping them again
    - All containers should have restart: always so if they are down is for a reason

--- scripts/apps-update.sh
+++ scripts/apps-update.sh
@@ -4,12 +4,14 @@ base_dir="/Apps/portainer/compose"
 failure=0
 for sub_dir in "$base_dir"/*/; do
   if [ -f "${sub_dir}docker-compose.yml" ]; then
-    if docker compose -f "${sub_dir}docker-compose.yml" pull; then
-      if ! docker compose -f "${sub_dir}docker-compose.yml" up -d; then
+    if docker compose -f "${sub_dir}docker-compose.yml" ps --services --filter "status=running" | grep -q .; then
+      if docker compose -f "${sub_dir}docker-compose.yml" pull; then
+        if ! docker compose -f "${sub_dir}docker-compose.yml" up -d; then
+          failure=1
+        fi
+      else
         failure=1
       fi
-    else
-      failure=1
     fi
   fi
 done
